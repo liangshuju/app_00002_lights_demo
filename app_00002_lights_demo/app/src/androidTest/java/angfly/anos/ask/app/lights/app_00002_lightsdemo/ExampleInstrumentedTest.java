3386b7c378aa2c990e1204924baaf95c12705087
liangshuju <angfly.100ask>
2018-07-04 14:31:46 +0800

    v1.0.0 init app 00001 lights code.

---
+++ app_00002_lights_demo/app/src/androidTest/java/angfly/anos/ask/app/lights/app_00002_lightsdemo/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package angfly.anos.ask.app.lights.app_00002_lightsdemo;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("angfly.anos.ask.app.lights.app_00002_lightsdemo", appContext.getPackageName());
+    }
+}
---
+++ app_00002_lights_demo/app/src/main/java/angfly/anos/ask/app/lights/app_00002_lightsdemo/MainAppLightsActivity.java
@@ -0,0 +1,68 @@
+package angfly.anos.ask.app.lights.app_00002_lightsdemo;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+public class MainAppLightsActivity extends Activity {
+
+    private Button mLightButton = null;
+
+    boolean flashing = false;
+
+    final private int LED_NOTIFICATION_ID = 123;
+
+    private Handler mLightHandler = new Handler();
+
+    private LightRunnable mLightRunnable = new LightRunnable();
+
+    class LightRunnable implements Runnable {
+        @Override
+        public void run() {
+            if (flashing) {
+                flashingLight();
+            } else {
+                ClearLed();
+            }
+        }
+    }
+
+    private void ClearLed() {
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.cancel(LED_NOTIFICATION_ID);
+    }
+
+    private void flashingLight() {
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification mNotification = new Notification();
+        mNotification.flags = Notification.FLAG_SHOW_LIGHTS;
+        mNotification.ledARGB = 0xFF0000FF;
+        mNotification.ledOnMS = 100;
+        mNotification.ledOffMS = 100;
+        mNotificationManager.notify(LED_NOTIFICATION_ID, mNotification);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_app_lights);
+
+        mLightButton = (Button) findViewById(R.id.button_flash);
+        mLightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashing = !flashing;
+                if (flashing) {
+                    mLightButton.setText("Stop Flashing the Light");
+                } else {
+                    mLightButton.setText("Flashing Light at 20s");
+                }
+
+            }
+        });
+    }
+}
